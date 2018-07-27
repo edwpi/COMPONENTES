439414a649939409d2602744dc8333a53cb6dba4
Edwin Pinilla <gustavo.rivera@fuac.edu.co>
2018-07-26 21:32:00 -0500

    una pequeña prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+//import { NgModule } from '@angular/forms';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -10,6 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    NgModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,94 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  Usuario: string="";
+  Contrasena: string="";
+  constructor(public navCtrl: NavController,public alerCtrl: AlertController) {
 
   }
+  /*ingreso() {
+    const alert = this.alerCtrl.create({
+      title: 'Mensaje',
+      subTitle: 'Acceso Denegado  ' + this.Usuario,
+      buttons: ['OK']
+    });
+    alert.present();
+  }*/
 
+  validar()
+  {
+    if (this.Usuario=="")
+    {
+      const alert = this.alerCtrl.create({
+        title: 'Mensaje',
+        subTitle: 'Ingrese Usuario',
+        buttons: ['Volver']
+      });
+     alert.present();
+    }
+    else if(this.Contrasena=="")
+    {
+      const alert = this.alerCtrl.create({
+        title: 'Mensaje',
+        subTitle: 'Ingrese contraseña',
+        buttons: ['Volver']
+      });
+    alert.present();
+
+    }
+    else
+    {
+      const alert = this.alerCtrl.create({
+        title: 'Mensaje',
+        subTitle: 'Bienvenido  ' + this.Usuario,
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+    
+  }
+
+  recupera() {
+    let confirm = this.alerCtrl.create({
+      title: 'Recuperar contraseña',
+      message: 'Ingrese el correo donde desea recibir la contraseña',
+      inputs: [
+        {
+          name: 'Correo',
+          placeholder: 'Correo'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Aceptar',
+          handler: correo => {
+            this.envio(correo.Correo);
+            //console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    confirm.present()
+  }
+  envio(correo) {
+    const alert = this.alerCtrl.create({
+      title: 'Envio',
+      subTitle: correo,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+ 
 }
